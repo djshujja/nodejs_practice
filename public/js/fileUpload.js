c5e30f78d7fb4de0877b103e5419605d71d8d8c2
djshujja <djshujja@gmail.com>
2020-07-26 19:11:42 +0500

    form setup styles

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -5,7 +5,7 @@ fp.registerPlugin(
   FilePondPluginFileEncode
 );
 fp.setOptions({
-  // stylePanelAspectRatio: 150 / 100,
+  stylePanelAspectRatio: 150 / 100,
   imageResizeTargeWidth: 100,
   imageResizeTargetHeight: 150,
 });
